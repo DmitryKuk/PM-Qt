45f3b128e373f2a36224be1b64b7def1c4311b5a
Dmitry Kukovinets <d1021976@yandex.ru>
2014-11-27 01:00:30 +0300

    Clear old files

--- password.cpp
+++
@@ -1,3 +0,0 @@
-// Author: Dmitry Kukovinets (d1021976@gmail.com)
-
-#include "password.h"
--- passwordtree.hpp
+++
@@ -1,6 +0,0 @@
-// Author: Dmitry Kukovinets (d1021976@gmail.com)
-
-inline PasswordTree::Node * PasswordTree::Node::parent() const
-{
-	return this->parent_;
-}
