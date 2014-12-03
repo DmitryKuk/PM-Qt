afa2ff210194fd90641a5d0ae0e43ed9a83a4a59
Dmitry Kukovinets <d1021976@yandex.ru>
2014-12-04 00:22:18 +0300

    interval_set improvements

--- cryptokernel/interval_set.h
+++ cryptokernel/interval_set.h
@@ -45,17 +45,29 @@ public:
 	inline interval_set<T, Compare, Alloc> & operator=(const interval_set<T, Compare, Alloc> &other);
 	
 	// Inserts new interval or element as [element, element + 1) interval and merges it.
-	// Correct hint speeds up searching of interval.
-	inline iter_bool_pair insert(const T &element, interval_set<T, Compare, Alloc>::iterator hint = this->end());
-	inline iter_bool_pair insert(const std::pair<T, T> &interval, interval_set<T, Compare, Alloc>::iterator hint = this->end());
+	// TIME: O(log N + 1) (amortized) [search + insert + merge]
+	inline void insert(const T &element);
+	void insert(const std::pair<T, T> &interval);
 	
 	// Removes object from correct interval. If interval comes empty, deletes it.
-	// Correct hint speeds up searching of interval.
+	// TIME: O(log N + 1) (amortized) [search + erase + insert]
 	void erase(const T &element);
+	
+	// Searches for element. If it exists, returns true. Otherwise, returns false.
+	// TIME: O(log N)
+	bool find(const T &element) const;
 protected:
+	// Finds interval containing element. Returning value:
+	// 1) element exists: (itetator to it, true)
+	// 2) element does not exist, but left interval exists: (iterator to left interval, false)
+	// 3) element does not exist and no correct interval exists: (this->end(), false)
+	// TIME: O(log N)
+	interval_set<T, Compare, Alloc>::iter_bool_pair interval_set::find_interval(const T &element);
+	
 	// Tries to merge 3 intervals: it and its both left and right neighbours.
 	// If merging was successfully, return iterator to the new interval and true.
 	// Otherwise, returns given iterator and false.
+	// TIME: O(1) (amortized)
 	inline iter_bool_pair interval_set<T, Compare, Alloc>::merge(interval_set<T, Compare, Alloc>::iterator it);
 };
 
--- cryptokernel/interval_set.hpp
+++ cryptokernel/interval_set.hpp
@@ -2,25 +2,25 @@
 
 // Iterators
 template<class T, class Compare, class Alloc>
-inline iterator interval_set<T, Compare, Alloc>::begin()
+inline interval_set<T, Compare, Alloc>::iterator interval_set<T, Compare, Alloc>::begin()
 {
 	return this->parent_container::begin();
 }
 
 template<class T, class Compare, class Alloc>
-inline const_iterator interval_set<T, Compare, Alloc>::begin() const
+inline interval_set<T, Compare, Alloc>::const_iterator interval_set<T, Compare, Alloc>::begin() const
 {
 	return this->parent_container::begin();
 }
 
 template<class T, class Compare, class Alloc>
-inline iterator interval_set<T, Compare, Alloc>::end()
+inline interval_set<T, Compare, Alloc>::iterator interval_set<T, Compare, Alloc>::end()
 {
 	return this->parent_container::end();
 }
 
 template<class T, class Compare, class Alloc>
-inline const_iterator interval_set<T, Compare, Alloc>::end() const
+inline interval_set<T, Compare, Alloc>::const_iterator interval_set<T, Compare, Alloc>::end() const
 {
 	return this->parent_container::end();
 }
@@ -29,7 +29,7 @@ inline const_iterator interval_set<T, Compare, Alloc>::end() const
 // Constructors and operator=()
 template<class T, class Compare, class Alloc>
 inline
-interval_set<T, Compare, Alloc>::::interval_set(const Compare &key_comp, const Alloc &alloc):
+interval_set<T, Compare, Alloc>::interval_set(const Compare &key_comp, const Alloc &alloc):
 	parent_container(key_comp, alloc)
 {}
 
@@ -50,74 +50,120 @@ interval_set<T, Compare, Alloc>::operator=(const interval_set<T, Compare, Alloc>
 
 
 // Inserts new interval or element as [element, element + 1) interval and merges it.
-// Correct hint speeds up searching of interval.
+// TIME: O(log N + 1) (amortized) [search + insert + merge]
 template<class T, class Compare, class Alloc>
 inline
-interval_set<T, Compare, Alloc>::iter_bool_pair
-interval_set::insert(const T &element,
-					 interval_set<T, Compare, Alloc>::iterator hint)
+void
+interval_set<T, Compare, Alloc>::insert(const T &element)
 {
 	auto p = std::make_pair(element, element);	// p = [element, element]
 	++p.second;									// p = [element, element + 1)
-	return this->insert(p, hint);
+	return this->insert(p);
 }
 
-template<class T, class Compare, class Alloc>
-inline
-interval_set<T, Compare, Alloc>::iter_bool_pair
-interval_set::insert(const std::pair<T, T> &interval,
-					 interval_set<T, Compare, Alloc>::iterator hint)
-{
-	
-}
-
-
-template<class T, class Compare, class Alloc>
-interval_set<T, Compare, Alloc>::iter_bool_pair
-interval_set::find_interval(const T &element)
-{
-	Compare cmp = this->key_comp();	// Comparator
-	auto it = this->lower_bound(element);	// Searching for correct interval
-	// element exists if: (it->first <= element && element < it->second) == true
-	// If element >= it->second, returns false: it (or ++it) is the nearest interval.
-	if (it != this->end() && !cmp(element, it->first)) {
-		if (cmp(element, it->second)) return std::make_pair(std::move(it), true);
-		return std::make_pair(std::move(it), false);
-	}
-	return std::make_pair(this->end(), false);	// element does not exist
-}
-
-
-// Removes object from correct interval. If interval comes empty, deletes it.
 template<class T, class Compare, class Alloc>
 void
-interval_set::erase(const T &element)
+interval_set<T, Compare, Alloc>::insert(const std::pair<T, T> &interval)
 {
 	Compare cmp = this->key_comp();	// Comparator
 	// Helper lambda-function. Returns true, if a equals to b, false otherwise.
 	static auto are_equal =
-		[&cmp] (const T &a,
-				const T &b)
-		-> bool
+		[&cmp] (const T &a, const T &b) -> bool
 		{
-			if (cmp(a, b) || cmp(b, a)) return false;
-			return true;
+			return (cmp(a, b) || cmp(b, a))? false: true;
+		};
+	
+	if (!cmp(interval.first, interval.second)) return;	// Checking interval
+	
+	auto p = this->find_interval(interval.first);
+	
+	if (p.second &&
+		are_equal(p.first->first, interval.first) &&
+		cmp(p.first->second, interval.second))	// Stretch founded interval to new size
+		p.first->second = interval.second;	// [5, 15) + [5, 20) => [5, 20)
+	else {	// Insert new interval
+		if (p.first != this->end()) ++p.first;	// p.first points to next interval
+		auto it = this->parent_container::insert(p.first, interval);
+		this->merge(it);	// Merge inserted interval with its neighbours
+	}
+}
+
+
+// Removes object from correct interval. If interval comes empty, deletes it.
+// TIME: O(log N + 1) (amortized) [search + erase + insert]
+template<class T, class Compare, class Alloc>
+void
+interval_set<T, Compare, Alloc>::erase(const T &element)
+{
+	Compare cmp = this->key_comp();	// Comparator
+	// Helper lambda-function. Returns true, if a equals to b, false otherwise.
+	static auto are_equal =
+		[&cmp] (const T &a, const T &b) -> bool
+		{
+			return (cmp(a, b) || cmp(b, a))? false: true;
 		};
 	
 	auto p = this->find_interval(element);
 	if (!p.second) return;	// element does not exist
 	
-	// Forking interval: [l1, r1) => [l1, element), [l2, r1); l2 = element + 1
-	T left = it->first;
-	it->first = element; ++(it->first);
-	
-	this->parent_container::insert(it, std::make_pair(left, element));
+	// Forking interval: [left, right) => [left, element), [element + 1, right)
+	if (are_equal(element, it->first)) {	// [left = element, right) => [element + 1, right)
+		auto interval = std::move(*it); ++interval.first;	// left => element + 1
+		it = this->parent_container::erase(it);	// Now it points to next interval
+		if (cmp(interval.first, interval.second))	// Check: interval is correct (non-zero size)
+			this->parent_container::insert(it, std::move(interval));
+	} else {
+		T element1 = element; ++element1;
+		if (are_equal(element1, it->second))	// [left, right) => [left, right - 1)
+			it->second = element;
+		else {	// [left, right) => [left, element), [element + 1, right)
+			auto it1 = it; ++it1;
+			this->parent_container::emplace_hint(it1, std::move(element1), std::move(it->second));
+			it->second = element;
+		}
+	}
+}
+
+
+// Searches for element. If it exists, returns true. Otherwise, returns false.
+// TIME: O(log N)
+template<class T, class Compare, class Alloc>
+bool
+interval_set<T, Compare, Alloc>::find(const T &element) const
+{
+	Compare cmp = this->key_comp();	// Comparator
+	auto it = this->lower_bound(element);	// Searching for correct interval
+	if (it == this->end() || !cmp(element, it->second))
+		return false;	// interval or element does not exist
+	return true;
+}
+
+
+// Finds interval containing element. Returning value:
+// 1) element exists: (itetator to it, true)
+// 2) element does not exist, but left interval exists: (iterator to left interval, false)
+// 3) element does not exist and no correct interval exists: (this->end(), false)
+// TIME: O(log N)
+template<class T, class Compare, class Alloc>
+interval_set<T, Compare, Alloc>::iter_bool_pair
+interval_set<T, Compare, Alloc>::find_interval(const T &element)
+{
+	Compare cmp = this->key_comp();	// Comparator
+	auto it = this->lower_bound(element);	// Searching for correct interval
+	// element exists if: (it->first <= element && element < it->second) == true
+	// If element >= it->second, returns false: it (or ++it) is the nearest interval.
+	if (it != this->end()) {
+		if (cmp(element, it->second)) return std::make_pair(std::move(it), true);
+		return std::make_pair(std::move(it), false);
+	}
+	return std::make_pair(std::move(this->end()), false);	// element does not exist
 }
 
 
 // Tries to merge 3 interval_set: it and its both left and right neighbours.
 // If merging was successfully, return iterator to the new interval and true.
 // Otherwise, returns given iterator and false.
+// TIME: O(1) (amortized)
 template<class T, class Compare, class Alloc>
 inline
 interval_set<T, Compare, Alloc>::iter_bool_pair
@@ -143,7 +189,7 @@ interval_set<T, Compare, Alloc>::merge(interval_set<T, Compare, Alloc>::iterator
 	// NOTE: Does NOT check iterators for equal to end().
 	static auto merge_with_right =
 		[this] (interval_set<T, Compare, Alloc>::iterator left)
-interval_set<T, Compare, Alloc>::iter_bool_pair
+		-> interval_set<T, Compare, Alloc>::iter_bool_pair
 		{
 			auto right = left; ++right;
 			std::pair<T, T> p = *left;
