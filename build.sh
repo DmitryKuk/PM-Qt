31a8c706d00fb4a289c6585b2360d0498752f24d
Dmitry Kukovinets <d1021976@yandex.ru>
2014-12-26 01:20:36 +0300

    build.sh improved.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Author: Dmitry Kukovinets (d1021976@gmail.com)
 
-# Simple script to run all
+# Simple script to build and run all
 
 if (( $# != 0 && $# != 1 )); then
 	echo -e "Usage:\n\t$0 [run]"
@@ -12,11 +12,17 @@ fi
 qmake -Wall && make
 STATUS="$?"
 
-if [ "X${STATUS}X" == "X0X" ] && [ "X$1X" == "XrunX" ]; then
-	TARGET="$( uname )"
-	case "$( uname )" in
-		"Darwin") open PM.app/ ;;
-		"Linux") ./PM ;;
-		*) echo -e "Sorry, I don\'t know how to build it for $TARGET.\nSee \"build.sh\" for more information."
-	esac
+if [ "X$1" == "Xrun" ]; then
+	if [ "X$STATUS" == "X0" ]; then
+		TARGET_SYSTEM="$( uname )"
+		case "$( uname )" in
+			"Darwin") open PM.app/ ;;
+			"Linux") ./PM ;;
+			"Windows") echo -e "It's a joke I hope: uname says this is Windows OS.\nI can\'t run output file." ;;
+			*) echo -e "Sorry, I don't know how to build it for target system \"$TARGET_SYSTEM\".\nSee \"build.sh\" for more information."
+		esac
+	else
+		echo "Build failed, it's impossible to run output file."
+		exit "$STATUS"
+	fi
 fi
